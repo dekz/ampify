a302d89a10dc03667f94e960a0d64981a642b523
Jacob Evans <jacob@dekz.net>
2013-08-17 14:44:55 +1000

    remove test, add to ampify.rb

---
+++ lib/ampify.rb
@@ -0,0 +1,11 @@
+require 'bancamp_api'
+require 'yaml'
+require 'bandcamp_api'
+require 'pp'
+
+sensitive = YAML::load File.read './sensitive.yml'
+
+Bandcamp.config.api_key = sensitive[:api_key]
+
+pp Bandcamp.search "tycho"
+
