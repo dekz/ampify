c67ce36ece7cabe53841c6c3e2dbf6ca4bed986f
Nathan Rashleigh <nathan@payphone.local>
2013-08-17 15:54:43 +1000

    bootstrap

--- lib/apps/ampify.rb
+++ lib/apps/ampify.rb
@@ -2,14 +2,18 @@ require 'yaml'
 require 'sinatra'
 require 'bandcamp_api'
 require 'pry'
+require 'haml'
 
 $sensitive = YAML::load File.read './sensitive.yml'
 Bandcamp.config.api_key = $sensitive[:api_key]
 
+get '/' do
+  haml :index
+end
+
 get '/artist/:id' do
-  content_type :json
   result = Bandcamp.get.band params[:id]
-  result.to_json
+  haml :artist, :locals => {:artist => result}
 end
 
 get '/search/artist/:text' do
